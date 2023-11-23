7237b5f8e29b0a88dce3708fe6719a7a00ddd469
yanomasao <masao.yano@gmail.com>
2023-11-23 12:51:37 +0900

    ポートの変更

--- todo-react/vite.config.js
+++ todo-react/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 9120,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9121',
+        changeOrigin: true,
+      },
+    },
+  },
 })
