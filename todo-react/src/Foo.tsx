ad3cf5add8b0ab4198fba8a4c191835bd9674a3c
yanomasao <masao.yano@gmail.com>
2023-11-23 14:11:12 +0900

    axios追加

--- todo-react/src/Foo.tsx
+++ todo-react/src/Foo.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function foo() {
-    return <div>foo</div>
-}
+const Foo = () => {
+  // try {
+  //     const res = axios.get('/api/hello')
+  //     return <div>res.data</div>
+  // } catch (e) {
+  //     console.log(e)
+  // }
 
-export default foo
+  const [res, setRes] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("/api/hello")
+      .then(
+        (response) => {
+          setRes(response.data)
+        console.log(res)
+        })
+      .catch((err) => console.log(err));
+  });
+  if (res === null) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>{res}</div>;
+};
+
+export default Foo;
