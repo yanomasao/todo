ae2f209c84f4e04029b24b316da5725f06f3dd9e
yanomasao <masao.yano@gmail.com>
2023-11-23 19:48:22 +0900

    テーブルコンポーネント作成

---
+++ todo-react/src/TodoTable.tsx
@@ -0,0 +1,59 @@
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Todo } from "./if/todo";  // Todoインターフェースをインポートします
+import { todo } from "node:test";
+
+const TodoTable = () => {
+    //todos に型を定義するには？
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get("/api/todo");
+                console.log(res.data);
+                setTodos(res.data);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+
+        fetchData();
+    }, []);
+    console.log(todos)
+
+    if (todos === null) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <table>
+                <tr>
+                    <th>id</th>
+                    <th>title</th>
+                    <th>description</th>
+                    <th>status</th>
+                    <th>created_at</th>
+                    <th>updated_at</th>
+                </tr>
+                {todos.map((todo) => (
+                    <tr>
+                        <td>{todo['id']}</td>
+                        <td>{todo['title']}</td>
+                        <td>{todo['description']}</td>
+                        <td>{todo['status']}</td>
+                        <td>{todo['created_at']}</td>
+                        <td>{todo['created_by']}</td>
+                        <td>{todo['updated_at']}</td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    );
+    // return <div>FOOO</div>;
+    // return <div>{JSON.stringify(apiMessage)}</div>;
+};
+
+export default TodoTable;
