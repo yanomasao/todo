0f89e6ebbabb9e94823b9009157c343d6f00e485
yanomasao <masao.yano@gmail.com>
2023-11-23 12:38:22 +0900

    axumに変更

--- todo-rust/src/main.rs
+++ todo-rust/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+// use axum::{handler::get, Router};
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    // ルートを作成
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // サーバーを起動
+    axum::Server::bind(&"0.0.0.0:9121".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
