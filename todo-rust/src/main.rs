9ae8843626635498be6bd855428bf1eb18d2f65b
yanomasao <masao.yano@gmail.com>
2023-11-23 12:56:00 +0900

    route追加

--- todo-rust/src/main.rs
+++ todo-rust/src/main.rs
@@ -4,7 +4,8 @@ use axum::{Router, routing::get};
 #[tokio::main]
 async fn main() {
     // ルートを作成
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }))
+        .route("/api/hello", get(|| async { "Hello, API!" }));
 
     // サーバーを起動
     axum::Server::bind(&"0.0.0.0:9121".parse().unwrap())
