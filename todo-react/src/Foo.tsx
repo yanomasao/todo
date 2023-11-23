b777861b6af2c6ed834ed5e4b8c8ab1847498500
yanomasao <masao.yano@gmail.com>
2023-11-23 14:25:22 +0900

    APIの結果を表示

--- todo-react/src/Foo.tsx
+++ todo-react/src/Foo.tsx
@@ -2,30 +2,24 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Foo = () => {
-  // try {
-  //     const res = axios.get('/api/hello')
-  //     return <div>res.data</div>
-  // } catch (e) {
-  //     console.log(e)
-  // }
-
-  const [res, setRes] = useState(null);
+  const [apiMessage, setApiMessage] = useState(null);
 
   useEffect(() => {
     axios
       .get("/api/hello")
-      .then(
-        (response) => {
-          setRes(response.data)
-        console.log(res)
-        })
-      .catch((err) => console.log(err));
-  });
-  if (res === null) {
+      .then((res) => {
+        console.log(res.data);
+        setApiMessage(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  })
+  if (apiMessage === null) {
     return <div>Loading...</div>;
   }
 
-  return <div>{res}</div>;
+  return <div>{apiMessage}</div>;
 };
 
 export default Foo;
